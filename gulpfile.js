0e6919da552a289db18f4d86a95e38de9b6b2403
Tomasz Kuchne <tomasz.kuchne@mediaambassador.com>
2018-06-20 15:17:49 +0200

    Build with sass + update package

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 const gulp = require('gulp');
 const inlineNg2Template = require('gulp-inline-ng2-template');
 var runSequence = require('run-sequence');
+var sass = require('node-sass');
+
+const styleProcessor = (stylePath, ext, styleFile, callback) => {
+  if (ext[0] === '.scss') {
+      let sassObj = sass.renderSync({ file: stylePath });
+      if (sassObj && sassObj['css']){
+      styleFile = sassObj.css.toString('utf8');
+      }
+  }
+
+  return callback(null, styleFile);
+};
 
 gulp.task('inline-build-templates', function() {
     return gulp.src(['./src/app/common/**/*.ts', '!./src/app/common/**/*.spec.ts'])
         .pipe(inlineNg2Template({
-            target: 'es5',
-            useRelativePaths: true
+          target: 'es5',
+          useRelativePaths: true,
+          styleProcessor: styleProcessor
         }))
         .pipe(gulp.dest('./build'));
 });
